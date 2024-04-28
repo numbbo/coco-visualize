05007e0ea6ead386842fcef905c1c1633b47614e
Olaf Mersmann <olafm@p-value.net>
2024-04-28 22:28:54 +0200

    Add function to change lightness of color.

---
+++ src/cocoviz/utilities.py
@@ -0,0 +1,9 @@
+import colorsys
+
+from typing import List
+
+
+def scale_lightness(rgb: List[float], scale_l: float):
+    """Scale lightness of an RGB color"""
+    hue, lightness, saturation = colorsys.rgb_to_hls(*rgb)
+    return colorsys.hls_to_rgb(hue, min(1, lightness * scale_l), saturation)
