686dbb2c68cbc8481059ddee3f3d0dce1eded293
Olaf Mersmann <Olaf.Mersmann@DACS.int>
2024-07-04 23:12:33 +0200

    Add minimal tests

---
+++ tests/test_result.py
@@ -0,0 +1,45 @@
+import logging
+import polars as pl
+
+from polars.testing import assert_frame_equal
+
+from cocoviz.types import Result, ProblemDescription
+
+
+HV_a1 = {
+    "fevals":      [  1,   2,   3,  10,  20,  50, 100],
+    "hypervolume": [ 10,  20,  30,  40,  50,  60,  70],
+    "r2":          [ 20,  40,  60,  80, 100, 120, 140]
+}
+
+HV_a1_nofevals = {
+    "nofevals":      [  1,   2,   3,  10,  20,  50, 100],
+    "hypervolume": [ 10,  20,  30,  40,  50,  60,  70],
+    "r2":          [ 20,  40,  60,  80, 100, 120, 140]
+}
+
+pd_f1 = ProblemDescription("f1", "i1", 10, 2)
+
+
+def test_fevals_column(caplog):
+    with caplog.at_level(logging.WARNING):    
+        res = Result("a1", pd_f1, HV_a1_nofevals) # noqa: F841
+    assert "Assuming first column" in caplog.text
+
+
+def test_at_indicator():
+    targets = [15, 25, 35, 120]
+    
+
+    res = Result("a1", pd_f1, HV_a1)
+    
+    ind = res.at_indicator("hypervolume", targets)
+
+    true_ind = pl.DataFrame([
+        pl.Series("__fevals", [2.0, 3.0, 10.0, 100.0], dtype=pl.Float64),
+        pl.Series("hypervolume", [15, 25, 35, 120], dtype=pl.Int64),
+        pl.Series("__target_hit", [1.0, 1.0, 1.0, 0.0], dtype=pl.Float64),
+        pl.Series("__fevals_dim", [0.2, 0.30, 1.0, 10.0], dtype=pl.Float64),
+    ])
+    
+    assert_frame_equal(ind._data, true_ind)
---
+++ tests/test_resultset.py
@@ -0,0 +1,42 @@
+import pytest
+
+from cocoviz.exceptions import IndicatorMismatchException
+from cocoviz.types import Result, ResultSet, ProblemDescription
+
+
+HV_a1 = {
+    "fevals":      [  1,   2,   3,  10,  20,  50, 100],
+    "hypervolume": [ 10,  20,  30,  40,  50,  60,  70],
+    "r2":          [ 20,  40,  60,  80, 100, 120, 140]
+}
+
+HV_a2 = {
+    "fevals":      [  1,   2,   3,  10,  20,  50, 100],
+    "hypervolume": [ 12,  22,  32,  42,  52,  62,  72],
+    "r2":          [ 22,  42,  62,  82, 102, 122, 142]
+}
+
+HV_a2_nor2 = HV_a2.copy()
+del HV_a2_nor2["r2"] 
+
+
+def test_indicator_mismatch():
+    pd_f1 = ProblemDescription("f1", "i1", 10, 2)
+    r1 = Result("a1", pd_f1, HV_a1)
+    r2 = Result("a2", pd_f1, HV_a2_nor2)
+
+    with pytest.raises(IndicatorMismatchException):
+        rs = ResultSet([r1, r2])
+
+    with pytest.raises(IndicatorMismatchException):
+        rs = ResultSet([r1, r2])
+
+    with pytest.raises(IndicatorMismatchException):
+        rs = ResultSet()
+        rs.append(r1)
+        rs.append(r2)
+
+    with pytest.raises(IndicatorMismatchException):
+        rs = ResultSet()
+        rs.append(r2)
+        rs.append(r1)
